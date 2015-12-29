755b473b9b0dacde8e486850166a8777bd0b4158
kylewurtz <kpwurtz@gmail.com>
2015-12-28 17:34:42 -0700

    Continued data prep
    Continued investigating variables and began working on feature
    selection.

--- Code/Sample Model.R
+++ Code/Sample Model.R
@@ -1,10 +1,19 @@
 # ---- load packages ----
+# general
 require(plyr)
 require(dplyr)
+require(tidyr)
 require(magrittr)
 require(readr)
 require(Amelia)
 require(ggvis)
+require(scales)
+
+# modeling
+require(rpart)
+require(rattle)
+require(rpart.plot)
+require(RColorBrewer)
 
 # ---- read in train data set ----
 train = read_csv("../KaggleTitanic/Data/train.csv")
@@ -15,6 +24,15 @@ summary(train)
 
 # map missing data
 missmap(train)
+
+# check survival rate overall
+prop.table(table(train$Survived))
+
+# ---- read in test data set ----
+test = read_csv("../KaggleTitanic/Data/test.csv")
+
+# take an initial look at the data set
+glimpse(test)
 
 # ---- visualizations ----
 # see distribution of survived
@@ -91,3 +109,69 @@ train %>%
   ggvis(~Embarked, ~PercentSurvived) %>% 
   layer_bars()
 # takeaway: C more likely to survive (overlap with class?)
+
+# ---- "Everyone Dies" Model ----
+# see results on train
+train %<>%
+  mutate(SurvivedPred_allPerish = rep(0, nrow(train)),
+         PredCorrect_allPerish = ifelse(Survived == SurvivedPred_allPerish, 1, 0))
+testStat_allPerish = sum(train$PredCorrect_allPerish)/nrow(train)
+percent(testStat_allPerish)
+# correct 61.6% of the time
+
+# make same prediction on test for sample submission
+test %<>% 
+  mutate(Survived = rep(0, nrow(test)))
+
+# create sample submission
+submit = data.frame(
+  PassengerId = test$PassengerId,
+  Survived = test$Survived
+)
+
+# write output csv for submission
+# write_csv(submit, "../KaggleTitanic/Submissions/TheyAllPerish/theyallperish.csv")
+
+# ---- Gender Model ----
+# predict women survive
+# see results on train
+train %<>%
+  mutate(SurvivedPred_Gender = ifelse(Sex == "female", 1, 0),
+         PredCorrect_Gender = ifelse(Survived == SurvivedPred_Gender, 1, 0))
+testStat_Gender = sum(train$PredCorrect_Gender)/nrow(train)
+percent(testStat_Gender)
+# correct 78.7% of the time
+
+# ---- Decision Tree Model ----
+# start with a model of all predictors
+predictors = c("Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked")
+formula = as.formula(paste("Survived",paste(predictors, collapse=" + "),sep=" ~ "))
+fit_all = rpart(formula, train, method="class")
+fancyRpartPlot(fit_all)
+train %<>%
+  mutate(SurvivedPred_DT_All = predict(fit_all, train, type = "class"),
+         PredCorrect_DT_All = ifelse(Survived == SurvivedPred_DT_All, 1, 0))
+testStat_DT_All = sum(train$PredCorrect_DT_All)/nrow(train)
+percent(testStat_DT_All)
+# correct 84% of the time
+
+# ---- Feature Engineering ----
+# family size
+train %<>%
+  mutate(FamilySize = SibSp + Parch + 1)
+train %>% ggvis(~FamilySize) %>% layer_histograms()
+train %>% 
+  group_by(FamilySize) %>% 
+  summarize(PercentSurvived = mean(Survived)) %>% 
+  ggvis(~FamilySize, ~PercentSurvived) %>% 
+  layer_bars()
+# nothing too dramatic here...
+
+# titles
+head(train$Name)
+train$Name[514]
+train$Name[514] = "Rothschild, Mrs. Martin (Elizabeth L Barrett)"
+train %<>% separate(Name,c("Last","Title","FirstMiddle"),sep="[,.].",remove=FALSE)
+train %<>%
+  mutate(Title = as.factor(Title))
+summary(train$Title)
